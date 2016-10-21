d973f8a0628025afe682aca21e3c3c1f12928cb4
Ubuntu <ubuntu@ip-172-31-17-191.ap-southeast-1.compute.internal>
2016-10-21 00:24:27 +0000

    First Commit

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+class helloworld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");	
+		System.out.println("Hello World again");	
+		System.out.println("Hello World 3rd time");	
+	}
+}
