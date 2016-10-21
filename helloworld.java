66bfa35702f2a6ab9a8aa88678478a1fd02dc6e4
Ubuntu <ubuntu@ip-172-31-17-191.ap-southeast-1.compute.internal>
2016-10-21 00:51:19 +0000

    changes committed

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,7 @@ class helloworld
 		System.out.println("Hello World");	
 		System.out.println("Hello World again");	
 		System.out.println("Hello World 3rd time");	
+		System.out.println("Hello changing World");	
+	
 	}
 }
